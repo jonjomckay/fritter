ad4791a845ceaaa4fea77f26017c7f25daf54edf
Jonjo McKay <jonjo@jonjomckay.com>
2021-02-07 16:46:47 +0000

    Added support for selecting which Nitter instances to use, and randomly selecting one for requests

--- lib/options.dart
+++ lib/options.dart
@@ -2,62 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:preferences/preferences.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'models.dart';
-
-var countries = {
-  'BG': Country('\u{1F1E7}\u{1F1EC}', 'Bulgaria'),
-  'DE': Country('\u{1F1E9}\u{1F1EA}', 'Germany'),
-  'ES': Country('\u{1F1EA}\u{1F1F8}', 'Spain'),
-  'FI': Country('\u{1F1EB}\u{1F1EE}', 'Finland'),
-  'FR': Country('\u{1F1EB}\u{1F1F7}', 'France'),
-  'IN': Country('\u{1F1EE}\u{1F1F3}', 'India'),
-  'NL': Country('\u{1F1F3}\u{1F1F1}', 'Netherlands'),
-  'RS': Country('\u{1F1F7}\u{1F1F8}', 'Serbia'),
-  'US': Country('\u{1F1FA}\u{1F1F8}', 'United States of America'),
-};
-
-var instances = {
-  'BG': [
-    Instance('nitter.himiko.cloud', '\u{1F1E7}\u{1F1EC}'),
-  ],
-  'DE': [
-    Instance('nitter.net', '\u{1F1E9}\u{1F1EA}'),
-    Instance('nitter.nixnet.services', '\u{1F1E9}\u{1F1EA}'),
-    Instance('nitter.13ad.de', '\u{1F1E9}\u{1F1EA}'),
-    Instance('nitter.pussthecat.org', '\u{1F1E9}\u{1F1EA}'),
-    Instance('nitter.mastodont.cat', '\u{1F1E9}\u{1F1EA}'),
-    Instance('nitter.cattube.org', '\u{1F1E9}\u{1F1EA}'),
-    Instance('nitter.namazso.eu', '\u{1F1E9}\u{1F1EA}'),
-  ],
-  'FI': [
-    Instance('nitter.eu', '\u{1F1EB}\u{1F1EE}'),
-  ],
-  'FR': [
-    Instance('nitter.42l.fr', '\u{1F1EB}\u{1F1F7}'),
-    Instance('nitter.tedomum.net', '\u{1F1EB}\u{1F1F7}'),
-    Instance('nitter.fdn.fr', '\u{1F1EB}\u{1F1F7}'),
-    Instance('nitter.ethibox.fr', '\u{1F1EB}\u{1F1F7}'),
-  ],
-  'IN': [
-    Instance('nitter.kavin.rocks', '\u{1F1EE}\u{1F1F3}'),
-  ],
-  'NL': [
-    Instance('nitter.unixfox.eu', '\u{1F1F3}\u{1F1F1}'),
-  ],
-  'RS': [
-    Instance('nitter.cc', '\u{1F1F7}\u{1F1F8}'),
-  ],
-  'ES': [
-    Instance('nitter.vxempire.xyz', '\u{1F1EA}\u{1F1F8}'),
-  ],
-  'US': [
-    Instance('nitter.dark.fail', '\u{1F1FA}\u{1F1F8}'),
-    Instance('nitter.1d4.us', '\u{1F1FA}\u{1F1F8}'),
-    Instance('tweet.lambda.dance', '\u{1F1FA}\u{1F1F8}'),
-    Instance('nitter.weaponizedhumiliation.com', '\u{1F1FA}\u{1F1F8}'),
-    Instance('nitter.domain.glass', '\u{1F1FA}\u{1F1F8}'),
-  ],
-};
+import 'constants.dart';
 
 class OptionsScreen extends StatefulWidget {
   @override
@@ -85,8 +30,8 @@ class _OptionsScreenState extends State<OptionsScreen> {
   Widget build(BuildContext context) {
     List<Widget> instanceCheckboxes = [];
 
-    for (var entry in instances.entries) {
-      var country = countries[entry.key];
+    for (var entry in INSTANCES.entries) {
+      var country = COUNTRIES[entry.key];
 
       instanceCheckboxes.add(PreferenceText('${country.name}',
           leading: Column(
