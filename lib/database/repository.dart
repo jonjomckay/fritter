fa5d966ae8eea9dfebbb3c5607ae30832c785496
Jonjo McKay <jonjo@jonjomckay.com>
2021-06-18 21:41:24 +0100

    Fixed migration incompatible with older SQLite versions

--- lib/database/repository.dart
+++ lib/database/repository.dart
@@ -135,9 +135,11 @@ class Repository {
         SqlMigration('ALTER TABLE $TABLE_SUBSCRIPTION ADD COLUMN verified BOOLEAN DEFAULT false', reverseSql: 'ALTER TABLE $TABLE_SUBSCRIPTION DROP COLUMN verified')
       ],
       15: [
-        // Re-apply migration 14, as it looks like it didn't apply for some people
-        SqlMigration('ALTER TABLE $TABLE_SUBSCRIPTION DROP COLUMN verified'),
-        SqlMigration('ALTER TABLE $TABLE_SUBSCRIPTION ADD COLUMN verified BOOLEAN DEFAULT false', reverseSql: 'ALTER TABLE $TABLE_SUBSCRIPTION DROP COLUMN verified')
+        // Re-apply migration 14 in a different way, as it looks like it didn't apply for some people
+        SqlMigration('ALTER TABLE $TABLE_SUBSCRIPTION RENAME TO ${TABLE_SUBSCRIPTION}_old'),
+        SqlMigration('CREATE TABLE $TABLE_SUBSCRIPTION (id VARCHAR PRIMARY KEY, screen_name VARCHAR, name VARCHAR, profile_image_url_https VARCHAR, verified BOOLEAN DEFAULT false, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)'),
+        SqlMigration('INSERT INTO $TABLE_SUBSCRIPTION (id, screen_name, name, profile_image_url_https, verified, created_at) SELECT id, screen_name, name, profile_image_url_https, false, created_at FROM ${TABLE_SUBSCRIPTION}_old'),
+        SqlMigration('DROP TABLE ${TABLE_SUBSCRIPTION}_old'),
       ]
     });
 
