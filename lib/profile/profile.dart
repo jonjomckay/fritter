8926271bb4f796ac516dde00c5d7506bfb25d2ce
Jonjo McKay <jonjo@jonjomckay.com>
2021-10-19 20:37:03 +0100

    Add missing fields to the profile screen (#204)
    * WIP: Added in-progress improved profile screen UX
    
    * WIP: Moved things around in the new profile header
    
    * WIP: Made profile description links clickable
    
    * WIP: Made profile headers more consistent on different device sizes
    
    * WIP: Made profile appbar height work across multiple devices
    
    * WIP: Removed ColouredTabBar
    
    * WIP: Made profile headers work in light and dark mode
    
    * Added changelog note about the new profile screen

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -2,14 +2,19 @@ import 'dart:async';
 
 import 'package:dart_twitter_api/twitter_api.dart';
 import 'package:extended_image/extended_image.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:fritter/client.dart';
+import 'package:fritter/constants.dart';
+import 'package:fritter/home/_search.dart';
 import 'package:fritter/profile/_follows.dart';
 import 'package:fritter/profile/_tweets.dart';
 import 'package:fritter/ui/errors.dart';
 import 'package:fritter/ui/futures.dart';
 import 'package:fritter/user.dart';
-import 'package:logging/logging.dart';
+import 'package:intl/intl.dart';
+import 'package:measured_size/measured_size.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -22,7 +27,6 @@ class ProfileScreen extends StatelessWidget {
   }
 }
 
-
 class _ProfileScreen extends StatefulWidget {
   final String username;
 
@@ -33,8 +37,6 @@ class _ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<_ProfileScreen> {
-  static final log = Logger('_ProfileScreenState');
-
   late Future<User> _future;
   
   @override
@@ -79,6 +81,9 @@ class ProfileScreenBody extends StatefulWidget {
 class _ProfileScreenBodyState extends State<ProfileScreenBody> with TickerProviderStateMixin {
   late TabController _tabController;
 
+  double descriptionHeight = 0;
+  double metadataHeight = 0;
+
   @override
   void initState() {
     super.initState();
@@ -86,17 +91,79 @@ class _ProfileScreenBodyState extends State<ProfileScreenBody> with TickerProvid
     _tabController = TabController(length: 5, vsync: this);
   }
 
+  List<InlineSpan> _addLinksToText(BuildContext context, String content) {
+    List<InlineSpan> contentWidgets = [];
+
+    // Split the string by any mentions or hashtags, and turn those into links
+    content.splitMapJoin(RegExp(r'(#|(?<=\W|^)@)\w+'),
+        onMatch: (match) {
+          var full = match.group(0);
+          var type = match.group(1);
+          if (type == null || full == null) {
+            return '';
+          }
+
+          var onTap = () async {};
+          if (type == '#') {
+            onTap = () async {
+              await showSearch(
+                context: context,
+                delegate: TweetSearchDelegate(
+                    initialTab: 1
+                ),
+                query: full
+              );
+            };
+          }
+
+          if (type == '@') {
+            onTap = () async {
+              Navigator.pushNamed(context, ROUTE_PROFILE, arguments: full.substring(1));
+            };
+          }
+
+          contentWidgets.add(TextSpan(
+              text: full,
+              style: TextStyle(color: Theme.of(context).accentColor),
+              recognizer: TapGestureRecognizer()
+                ..onTap = onTap
+          ));
+
+          return type;
+        },
+        onNonMatch: (text) {
+          contentWidgets.add(TextSpan(
+              text: text
+          ));
+
+          return text;
+        }
+    );
+
+    return contentWidgets;
+  }
+
   @override
   Widget build(BuildContext context) {
     // Make the app bar height the correct aspect ratio based on the header image size (1500x500)
-    var deviceSize = MediaQuery.of(context).size;
-    var appBarHeight = deviceSize.width * (500 / 1500);
+    var mediaQuery = MediaQuery.of(context);
+    var deviceSize = mediaQuery.size;
+    var bannerHeight = deviceSize.width * (500 / 1500);
+    var avatarHeight = 80;
+
+    var profileImageTop = bannerHeight + 16 - 36 - mediaQuery.padding.top;
+    var profileStuffTop = bannerHeight;
+
+    var theme = Theme.of(context);
 
     var profile = widget.user;
     var banner = profile.profileBannerUrl;
     var bannerImage = banner == null
-        ? Container()
-        : ExtendedImage.network(banner, fit: BoxFit.cover, height: appBarHeight, cache: true);
+        ? Container(height: bannerHeight, color: Colors.white)
+        : ExtendedImage.network(banner, fit: BoxFit.fitWidth, height: bannerHeight);
+
+    // The height of the app bar should be all the inner components, plus any margins
+    var appBarHeight = profileStuffTop + avatarHeight + metadataHeight + 48 + descriptionHeight;
 
     return Scaffold(
       body: NestedScrollView(
@@ -127,32 +194,166 @@ class _ProfileScreenBodyState extends State<ProfileScreenBody> with TickerProvid
                   Tab(child: Text('Followers', textAlign: TextAlign.center)),
                 ],
               ),
-              title: Row(
-                children: [
-                  Text(profile.name!),
-                  if (profile.verified ?? false)
-                    SizedBox(width: 6),
-                  if (profile.verified ?? false)
-                    Icon(Icons.verified, size: 24, color: Colors.white)
-                ],
-              ),
               flexibleSpace: FlexibleSpaceBar(
-                background: Stack(
-                  fit: StackFit.expand,
-                  children: <Widget>[
-                    bannerImage,
-                    const DecoratedBox(
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.bottomCenter,
-                          end: Alignment.topCenter,
-                          colors: <Color>[Color(0xBB000000), Color(0x50000000)],
+                background: SafeArea(
+                  child: DefaultTextStyle.merge(
+                    style: TextStyle(color: Colors.white),
+                    child: Stack(
+                      fit: StackFit.expand,
+                      children: <Widget>[
+                        Container(
+                          alignment: Alignment.topCenter,
+                          child: bannerImage
                         ),
-                      ),
+                        const DecoratedBox(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              begin: Alignment.bottomCenter,
+                              end: Alignment.topCenter,
+                              colors: <Color>[Color(0xDD000000), Color(0x80000000)],
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.symmetric(vertical: 16, horizontal: 0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Flexible(
+                                child: Container(
+                                  margin: EdgeInsets.fromLTRB(16, profileStuffTop, 16, 0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Container(
+                                        margin: EdgeInsets.only(bottom: 4),
+                                        child: Row(
+                                          children: [
+                                            Text(profile.name!, style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.w700
+                                            )),
+                                            if (profile.verified ?? false)
+                                              SizedBox(width: 6),
+                                            if (profile.verified ?? false)
+                                              Icon(Icons.verified, size: 24, color: Colors.blue)
+                                          ],
+                                        ),
+                                      ),
+                                      Container(
+                                        margin: EdgeInsets.only(bottom: 8),
+                                        child: Text('@${(profile.screenName!)}', style: TextStyle(
+                                            color: theme.primaryTextTheme.caption!.color
+                                        )),
+                                      ),
+                                      if (profile.description != null && profile.description!.isNotEmpty)
+                                        MeasuredSize(
+                                          onChange: (size) {
+                                            setState(() {
+                                              descriptionHeight = size.height;
+                                            });
+                                          },
+                                          child: Container(
+                                            margin: EdgeInsets.only(bottom: 4),
+                                            child: RichText(
+                                              maxLines: 3,
+                                              text: TextSpan(
+                                                style: TextStyle(
+                                                  height: 1.4
+                                                ),
+                                                children: _addLinksToText(context, profile.description!)
+                                              )
+                                            )
+                                          ),
+                                        ),
+                                      MeasuredSize(
+                                        onChange: (size) {
+                                          setState(() {
+                                            metadataHeight = size.height;
+                                          });
+                                        },
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          mainAxisAlignment: MainAxisAlignment.end,
+                                          children: [
+                                            if (profile.location != null && profile.location!.isNotEmpty)
+                                              Padding(
+                                                padding: EdgeInsets.symmetric(vertical: 3, horizontal: 0),
+                                                child: Row(
+                                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                                  children: [
+                                                    Icon(Icons.place, size: 12, color: Colors.white),
+                                                    SizedBox(width: 4),
+                                                    Text(profile.location!),
+                                                  ],
+                                                ),
+                                              ),
+                                            if (profile.url != null && profile.url!.isNotEmpty)
+                                              Padding(
+                                                padding: EdgeInsets.symmetric(vertical: 3, horizontal: 0),
+                                                child: Row(
+                                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                                  children: [
+                                                    Icon(Icons.link, size: 12, color: Colors.white),
+                                                    SizedBox(width: 4),
+                                                    Builder(builder: (context) {
+                                                      var url = profile.entities?.url?.urls
+                                                          ?.firstWhere((element) => element.url == profile.url);
+
+                                                      if (url == null) {
+                                                        return Container();
+                                                      }
+
+                                                      return InkWell(
+                                                        child: Text('${url.displayUrl!}', style: TextStyle(
+                                                          color: Colors.blue
+                                                        )),
+                                                        onTap: () => launch(url.expandedUrl!),
+                                                      );
+                                                    }),
+                                                  ],
+                                                ),
+                                              ),
+                                            if (profile.createdAt != null)
+                                              Padding(
+                                                padding: EdgeInsets.symmetric(vertical: 3, horizontal: 0),
+                                                child: Row(
+                                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                                  children: [
+                                                    Icon(Icons.calendar_today, size: 12, color: Colors.white),
+                                                    SizedBox(width: 4),
+                                                    Text('Joined ${DateFormat('MMMM yyyy').format(profile.createdAt!)}'),
+                                                  ],
+                                                ),
+                                              ),
+                                          ]
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+
+                        Container(
+                          alignment: Alignment.topRight,
+                          margin: EdgeInsets.fromLTRB(16, profileImageTop, 16, 16),
+                          child: CircleAvatar(
+                            radius: 50,
+                            backgroundColor: Colors.white,
+                            child: CircleAvatar(
+                              backgroundImage: createUserAvatar(profile.profileImageUrlHttps, 72).image,
+                              radius: 48,
+                            ),
+                          ),
+                        )
+                      ]
                     ),
-                  ],
-                ),
-              ),
+                  ),
+                )
+              )
             )
           ];
         },
