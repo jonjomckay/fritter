09ed18dae14f7b53074a6474a5edb2b04fb23373
Jonjo McKay <jonjo@jonjomckay.com>
2022-08-23 17:10:09 +0100

    Fixed not being able to double-tap images to zoom (#430)

--- lib/tweet/_photo.dart
+++ lib/tweet/_photo.dart
@@ -1,37 +1,69 @@
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 
-class TweetPhoto extends StatelessWidget {
+List<double> _doubleTapScales = <double>[1.0, 4.0];
+
+class TweetPhoto extends StatefulWidget {
   final String uri;
   final BoxFit fit;
   final String size;
-  final bool inPageView;
 
-  const TweetPhoto(
-      {Key? key, required this.uri, this.fit = BoxFit.fitWidth, required this.size, this.inPageView = false})
-      : super(key: key);
+  const TweetPhoto({Key? key, required this.uri, this.fit = BoxFit.fitWidth, required this.size}) : super(key: key);
+
+  @override
+  State<TweetPhoto> createState() => _TweetPhotoState();
+}
+
+class _TweetPhotoState extends State<TweetPhoto> with SingleTickerProviderStateMixin {
+  Animation<double>? _doubleClickAnimation;
+  late void Function() _doubleClickAnimationListener;
+  late final AnimationController _doubleClickAnimationController =
+      AnimationController(duration: const Duration(milliseconds: 150), vsync: this);
 
   @override
   Widget build(BuildContext context) {
     return ExtendedImage.network(
-      '$uri:$size',
+      '${widget.uri}:${widget.size}',
       cache: true,
       width: 5000,
       height: 5000,
-      fit: fit,
+      fit: widget.fit,
       mode: ExtendedImageMode.gesture,
       initGestureConfigHandler: (state) {
         return GestureConfig(
-          minScale: 1.0,
+          minScale: 0.9,
           animationMinScale: 0.7,
-          maxScale: 3.0,
-          animationMaxScale: 3.5,
+          maxScale: 4.0,
+          animationMaxScale: 4.0,
           speed: 1.0,
           inertialSpeed: 100.0,
           initialScale: 1.0,
-          inPageView: inPageView,
           initialAlignment: InitialAlignment.center,
         );
+      },
+      onDoubleTap: (ExtendedImageGestureState state) {
+        final Offset? pointerDownPosition = state.pointerDownPosition;
+        final double? begin = state.gestureDetails!.totalScale;
+        double end;
+
+        // Remove and stop any old animation
+        _doubleClickAnimation?.removeListener(_doubleClickAnimationListener);
+        _doubleClickAnimationController.stop();
+        _doubleClickAnimationController.reset();
+
+        if (begin == _doubleTapScales[0]) {
+          end = _doubleTapScales[1];
+        } else {
+          end = _doubleTapScales[0];
+        }
+
+        _doubleClickAnimationListener = () {
+          state.handleDoubleTap(scale: _doubleClickAnimation!.value, doubleTapPosition: pointerDownPosition);
+        };
+
+        _doubleClickAnimation = _doubleClickAnimationController.drive(Tween<double>(begin: begin, end: end));
+        _doubleClickAnimation!.addListener(_doubleClickAnimationListener);
+        _doubleClickAnimationController.forward();
       },
     );
   }
