55df2b2ecc3fa65d710556803cb568501b1711ee
Jonjo McKay <jonjo@jonjomckay.com>
2022-09-25 13:45:40 +0100

    Now using the correct Flutter version when building for the Play Store

--- build-play.sh
+++ build-play.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
-flutter packages pub run intl_utils:generate
-flutter build appbundle --dart-define=app.flavor=play --release --no-tree-shake-icons
+set -e
+
+fvm flutter packages pub run intl_utils:generate
+fvm flutter build appbundle --dart-define=app.flavor=play --release --no-tree-shake-icons
