05cbca0ff53a2697c1696ebc1a2722f937138156
Jonjo McKay <jonjo@jonjomckay.com>
2022-12-11 18:09:22 +0000

    Removed Dio, and also now caching translation responses

--- lib/utils/translation.dart
+++ lib/utils/translation.dart
@@ -1,8 +1,9 @@
-import 'package:catcher/catcher.dart';
-import 'package:dio/dio.dart';
+import 'dart:convert';
+
 import 'package:flutter_cache/flutter_cache.dart' as cache;
 import 'package:fritter/utils/iterables.dart';
 import 'package:fritter/utils/misc.dart';
+import 'package:http/http.dart' as http;
 import 'package:logging/logging.dart';
 
 Future<bool> isLanguageSupportedForTranslation(String lang) async {
@@ -27,16 +28,13 @@ class TranslationAPIResult {
 class TranslationAPI {
   static final log = Logger('TranslationAPI');
 
-  static final Dio _dio = Dio(BaseOptions(
-    baseUrl: 'https://libretranslate.de',
-  ));
-
   static Future<TranslationAPIResult> getSupportedLanguages() async {
     var key = 'translation.supported_languages';
 
-    // return cacheRequest(key, () async {
-    return sendRequest('/languages', 'Unable to get supported languages');
-    // });
+    return cacheRequest(key, () async {
+      var response = await http.get(Uri.https('libretranslate.de', '/languages'));
+      return await parseResponse(response, 'Unable to get supported languages');
+    });
   }
 
   static Future<TranslationAPIResult> translate(String id, List<String> text, String sourceLanguage) async {
@@ -52,10 +50,13 @@ class TranslationAPI {
 
     var key = 'translation.$sourceLanguage.$id';
 
-    // return cacheRequest(key, () async {
-    var res = await sendRequest('/translate', 'Unable to send translation request',
-        data: formData, options: Options(method: 'POST'));
-    // });
+    var res = await cacheRequest(key, () async {
+      var response = await http.post(Uri.https('libretranslate.de', '/translate'), body: jsonEncode(formData), headers: {
+        'Content-Type': 'application/json'
+      });
+
+      return await parseResponse(response, 'Unable to send translation request');
+    });
 
     if (res.success) {
       // We need to rehydrate the empty text parts that we stripped out earlier
@@ -80,65 +81,52 @@ class TranslationAPI {
     if (result != null) {
       // If we have a cached result, return it
       // TODO: Is this success always true? What if we cache an error?
-      return TranslationAPIResult(success: true, body: result);
+      return TranslationAPIResult(success: true, body: jsonDecode(result));
     }
 
     // Otherwise, make the request
     var response = await makeRequest();
     if (response.success) {
       // Cache the response if it's a successful one
-      var body = await cache.write(key, response.body);
+      var body = await cache.write(key, jsonEncode(response.body));
 
-      return TranslationAPIResult(success: true, body: body);
+      return TranslationAPIResult(success: true, body: response.body);
     }
 
     // Otherwise, we always want to return the error without caching
     return response;
   }
 
-  static Future<TranslationAPIResult> sendRequest(String path, String errorUnableTo,
-      {Options? options, Object? data}) async {
-    try {
-      var response = await _dio.request(path, data: data, options: options);
-
-      return TranslationAPIResult(success: response.statusCode == 200, body: response.data);
-    } on DioError catch (e, stackTrace) {
-      var response = e.response;
-      if (response == null) {
-        log.severe(errorUnableTo);
-        Catcher.reportCheckedError(e, stackTrace);
-        rethrow;
-      }
-
-      String message = '';
-
-      switch (response.statusCode) {
-        case 400:
-          RegExp languageNotSupported = RegExp(r"^\w+\ is\ not\ supported$");
-
-          var error = response.data['error'];
-          if (languageNotSupported.hasMatch(error)) {
-            message = 'Language $error';
-          } else {
-            message = error;
-          }
-          break;
-        case 403:
-          message = 'Error: Banned from translation API';
-          break;
-        case 429:
-          message = 'Error: Sending too many frequent translation requests';
-          break;
-        case 500:
-          message = 'Error: The translation API failed to translate the tweet';
-          break;
-      }
-
-      return TranslationAPIResult(success: false, body: response.data, errorMessage: message);
-    } catch (e, stackTrace) {
-      log.severe('Unable to translate');
-      Catcher.reportCheckedError(e, stackTrace);
-      rethrow;
+  static Future<TranslationAPIResult> parseResponse(http.Response response, String errorUnableTo) async {
+    var body = jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      return TranslationAPIResult(success: true, body: body);
     }
+
+    String message = '';
+
+    switch (response.statusCode) {
+      case 400:
+        RegExp languageNotSupported = RegExp(r"^\w+\ is\ not\ supported$");
+
+        var error = body['error'];
+        if (languageNotSupported.hasMatch(error)) {
+          message = 'Language $error';
+        } else {
+          message = error;
+        }
+        break;
+      case 403:
+        message = 'Error: Banned from translation API';
+        break;
+      case 429:
+        message = 'Error: Sending too many frequent translation requests';
+        break;
+      case 500:
+        message = 'Error: The translation API failed to translate the tweet';
+        break;
+    }
+
+    return TranslationAPIResult(success: false, body: body, errorMessage: message);
   }
 }
