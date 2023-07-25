1f309dee51fa60d551c45fc7222b76ac3fa4fac6
jfbriere <jeanfrancois.briere@creativaxion.com>
2023-07-25 14:59:28 -0400

    Fix the black recent screen mode.

--- android/app/src/main/kotlin/com/jonjomckay/fritter/MainActivity.kt
+++ android/app/src/main/kotlin/com/jonjomckay/fritter/MainActivity.kt
@@ -3,4 +3,13 @@ package com.jonjomckay.fritter
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+
+    protected override fun onPause() {
+        super.onPause()
+        try {
+            java.lang.Thread.sleep(200)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
 }
